e0dc92407c992526306194ed98bf707b0a6d9b69
ChesterTsai <tsaichester71@gmail.com>
2025-07-02 04:18:34 +0800

    Update choice.py
    fixed an issue where choice function won't work

--- cogs/choice.py
+++ cogs/choice.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 class Choice(commands.Cog):
     def __init__(self, bot):
@@ -7,7 +8,7 @@ class Choice(commands.Cog):
     
     @commands.command()
     async def choice(self, ctx, *, choices: str):
-        """選擇困難症，讓機器人幫你選擇，用法: choice (問題一) (問題二) [問題三] [問題四]..."""
+        """選擇困難症，讓機器人幫你選擇，用法: choice (問題一),(問題二),[問題三],[問題四]..."""
         responses = choices.split(',')
         await ctx.send(f'{random.choice(responses)} 是最棒的答案')
 
