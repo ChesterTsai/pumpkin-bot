0f42982487e38a2fa92e38c89665cff1ac9ba0f4
ChesterTsai <tsaichester71@gmail.com>
2026-01-22 21:43:44 +0800

    Add a tell time function
    - make bot change presence when time goes -> made public
    - make users able to subscribe/unsubscribe bot to tell how much time passed in a year.
    
    let's hope this function didn't go wrong cause I spent a lot more time than I original thought

---
+++ cogs/tellTime.py
@@ -0,0 +1,81 @@
+from discord.ext import commands, tasks
+import discord
+import datetime
+import time
+
+class tellTime(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.tellTimeLoop.start()
+
+    @commands.command()
+    async def subTime(self, ctx):
+        """訂閱機器人報時"""
+        channelID = ctx.channel.id
+        writeData(channelID)
+        await ctx.send("已訂閱機器人報時")
+
+    @commands.command()
+    async def unsubTime(self, ctx):
+        """取消訂閱機器人報時"""
+        channelID = ctx.channel.id
+        removeData(channelID)
+        await ctx.send("已取消訂閱機器人報時")
+
+    @tasks.loop(seconds = 1)
+    async def tellTimeLoop(self):
+        subbedChannel = readData()
+        if str(datetime.datetime.now().strftime("%m,%d,%H,%M,%S")) == "01,01,00,00,00":
+            msg = f'今年已經過了100%，新年快樂！'
+            await self.bot.change_presence(activity=discord.CustomActivity(name=msg))
+            for channelID in subbedChannel:
+                await self.bot.get_channel(channelID).send(f'[{datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")} INFO] {msg}')
+            time.sleep(1)
+        if int(time.time()) % 315576 == 0:
+            msg = f'今年已經過了{int((time.time() / 315576) % 100)}%'
+            await self.bot.change_presence(activity=discord.CustomActivity(name=msg))
+            for channelID in subbedChannel:
+                await self.bot.get_channel(channelID).send(f'[{datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")} INFO] {msg}')
+            time.sleep(1)
+
+    @tellTimeLoop.before_loop
+    async def before_loop(self):
+        await self.bot.wait_until_ready()
+
+async def setup(bot):
+    await bot.add_cog(tellTime(bot))
+
+def readData():
+    try:
+        f = open("./data/subTime.txt", "r", encoding="utf-8")
+        tmp = f.read()
+        data = tmp.split("\n")
+        f.close()
+    except FileNotFoundError:
+        f = open("./data/subTime.txt", "w", encoding='utf-8')
+        f.write("")
+        data = ""
+        f.close()
+    return data
+
+def writeData(channelID: int):
+    data = readData()
+
+    for i in data:
+        if i == str(channelID):
+            return
+
+    f = open("./data/subTime.txt", "a", encoding='utf-8')
+    f.write(str(channelID) + '\n')
+    f.close()
+
+def removeData(channelID: int):
+    data = readData()
+    if str(channelID) in data:
+        data.remove(str(channelID))
+    newData = ""
+    for i in data:
+        newData = newData + i + "\n"
+    f = open("./data/subTime.txt", "w", encoding='utf-8')
+    f.write(str(newData))
+    f.close()
